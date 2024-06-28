8ba03756f6edee87b132b01c5047b77396bd15d4
raudelf <rawdell@outlook.com>
2024-06-28 00:00:09 -0500

    uploaded an image of myself and added it to the homepage component + created an altText string to be used

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,11 +1,18 @@
 import BaseLayout from "../shared/BaseLayout";
+import { imageAltText } from "../shared/utils";
 
 function Homepage() {
   return (
     <BaseLayout>
       <div className="container homepage">
         <div className="homepage__content">
-          <div className="homepage__image-container"></div>
+          <div className="homepage__image-container">
+            <img
+              className="homepage__image"
+              src="/src/assets/raudelFlores.jpeg"
+              alt={imageAltText}
+            />
+          </div>
           <div className="homepage__text-container">
             <h1 className="homepage__title"></h1>
             <p className="homepage__text"></p>
--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -6,3 +6,11 @@
 export const githubLink = "https://github.com/raudelf";
 export const linkedInLink = "https://www.linkedin.com/in/raudelf/";
 export const contactEmail = "raudelf3@gmail.com";
+
+/*
+ *
+ * Homepage Image Alt Text
+ *
+ */
+export const imageAltText =
+  "Raudel sitting on a rock at a high vantage point, giving two thumbs up. He is wearing a red long-sleeve shirt, gray shorts, and a black cap. Behind him, there is a scenic view of a vast landscape with greenery, trees, and a distant city under a partly cloudy sky.";
