df9e6349dd45c0d7804e0dc2b9648d576a26e1e3
raudelf <rawdell@outlook.com>
2024-06-27 23:36:33 -0500

    cleaning up files, created utils file for links and footer and any futer additional variables needed on site, created error page when navigating to pages that don't exist and styles, condensing header and footer styles to one layout file

---
+++ src/shared/utils.ts
@@ -0,0 +1,8 @@
+/*
+ *
+ * Contact URLs
+ *
+ */
+export const githubLink = "https://github.com/raudelf";
+export const linkedInLink = "https://www.linkedin.com/in/raudelf/";
+export const contactEmail = "raudelf3@gmail.com";
