741a289da4fa2830d660260d82a94d16cddc6ca5
raudelf <rawdell@outlook.com>
2024-07-29 23:31:14 -0500

    src/pages/ProjectsPage.tsx

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,10 +1,28 @@
+import ProjectCard from "../components/ProjectCard";
 import BaseLayout from "../shared/BaseLayout";
+import { projects } from "../shared/utils";
 
 const ProjectsPage = () => {
   return (
     <BaseLayout>
-      <div className="container">
-        <h1>Projects</h1>
+      <div className="container projects-page">
+        <h1 className="projects-page__title">Projects</h1>
+
+        <ul className="projects-page__list">
+          {projects.projects.map((project, index) => {
+            return (
+              <li key={index} className="projects-page__list-item">
+                <ProjectCard
+                  clientLogo={project.clientLogo}
+                  clientName={project.clientName}
+                  clientSummary={project.clientSummary}
+                  clientSiteUrl={project.clientSiteUrl}
+                  descriptionText={project.descriptionText}
+                />
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </BaseLayout>
   );
