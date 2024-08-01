356106e825d93573dc3ab20e842f8d22c0e2801b
raudelf <rawdell@outlook.com>
2024-07-31 23:33:36 -0500

    refactoring project card component + adjusting styles and moving styles to new file

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,12 +1,4 @@
-import {
-  Button,
-  Dialog,
-  DialogTrigger,
-  Link,
-  Modal,
-} from "react-aria-components";
 import { ProjectCardProps } from "../shared/types";
-import CrossSVG from "../shared/svg/CrossSVG";
 
 const ProjectCard = (props: ProjectCardProps) => {
   const {
@@ -18,50 +10,25 @@ const ProjectCard = (props: ProjectCardProps) => {
   } = props;
 
   return (
-    <>
-      <DialogTrigger>
-        <Button className="projects-page__project-card">
-          <div className="projects-page__project-img-container">
-            <img
-              className="projects-page__project-img"
-              src={clientLogo}
-              aria-describedby="client-name"
-            />
-          </div>
-          <div className="projects-page__project-content">
-            <h2 id="client-name" className="projects-page__project-client">
-              {clientName}
-            </h2>
-            <p className="projects-page__project-text">{clientSummary}</p>
-          </div>
-        </Button>
-        <Modal className="projects-page__modal">
-          <Dialog>
-            {({ close }) => (
-              <div className="projects-page__modal-content">
-                <Button
-                  className="projects-page__close-modal-btn"
-                  onPress={close}
-                  aria-label="Close Modal"
-                >
-                  <CrossSVG className="projects-page__modal-svg" />
-                </Button>
-                <p className="projects-page__modal-text">{descriptionText}</p>
-
-                <Link
-                  href={clientSiteUrl}
-                  target="__blank"
-                  className="btn projects-page__modal-btn"
-                >
-                  Live Site
-                  <span className="visually-hidden">, opens in a new tab</span>
-                </Link>
-              </div>
-            )}
-          </Dialog>
-        </Modal>
-      </DialogTrigger>
-    </>
+    <div className="project-card__container">
+      <div className="project-card__img-container">
+        <img
+          className="project-card__img"
+          src={clientLogo}
+          aria-describedby="client-name"
+        />
+      </div>
+      <div className="project-card__content">
+        <a className="project-card__link" href={clientSiteUrl} target="_blank">
+          <h2 id="client-name" className="project-card__client">
+            {clientName}
+          </h2>
+          <span className="visually-hidden">, website opens in new tab</span>
+        </a>
+        <p className="project-card__text">{clientSummary}</p>
+        <p className="project-card__text">{descriptionText}</p>
+      </div>
+    </div>
   );
 };
 
